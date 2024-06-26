1da20f18403a6ec600a0191c30f8dae5b700d587
DanielMilesGH <daniel@milesfamily.name>
2024-06-25 21:52:19 -0500

    starting to give ability to test locally using pygame

--- src/python/robots.py
+++ src/python/robots.py
@@ -8,6 +8,9 @@ class BaseRobot():
         else:  
             self.x = x
             self.y = y 
+        
+        # images should be 300x300, making robot length 0.05 image
+        self.size = 15
 
     def get_pos(self) -> tuple[int,int]:
         return (self.x, self.y)
@@ -16,9 +19,17 @@ class BaseRobot():
     def get_y(self) -> int:
         return self.y 
 
+    # returns params to make pygame rect
+    def to_pygame_rect(self) -> tuple[int,int,int,int]:
+        return (self.x, self.y, self.size, self.size)
+    
     def update_pos(self, pos:tuple[int,int]):
         self.x=pos[0]
         self.y=pos[1]
+
+    def move(self, dx, dy):
+        self.x += dx 
+        self.y += dy
     
 
 
