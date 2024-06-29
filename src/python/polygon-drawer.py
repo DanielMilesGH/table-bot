13288cac337fd43209d3c980b892c41d08c8dc8a
DanielMilesGH <daniel@milesfamily.name>
2024-06-28 22:18:20 -0500

    added polygon drawer, and .txt files for line coordinates

---
+++ src/python/polygon-drawer.py
@@ -0,0 +1,52 @@
+import pygame
+import sys 
+from PIL import Image 
+import math
+# import robots
+# import src.python.robots
+from imports import robots
+
+clock = pygame.time.Clock()
+MAX_SIZE = (400,400)
+image = Image.open(r"data\\preset-images\\easy_image.jpg")
+image.thumbnail(MAX_SIZE)
+
+screen = pygame.display.set_mode(image.size)
+bg = pygame.image.fromstring(image.tobytes(), image.size, image.mode).convert()
+
+# Make the lines list a global variable, so the lines stay after reverting back to movement
+polys = []
+
+
+
+run = True
+robot = robots.PygameRobot(0,0)
+pygame.display.set_caption("Moving")
+points = []
+screen.blit(bg, (0,0))
+def sort_points(points):
+    # Calculate the center of the square
+    center_x = sum(p[0] for p in points) / 4
+    center_y = sum(p[1] for p in points) / 4
+
+    # Sort points based on the angle from the center
+    sorted_points = sorted(points, key=lambda p: math.atan2(p[1] - center_y, p[0] - center_x))
+
+    return sorted_points
+while run:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+ 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Left click for drawing
+            if event.button == 1:
+                points.append(event.pos)  # Record point 
+
+    if len(points) == 4:
+        pygame.draw.polygon(screen, (0,255,0), sort_points(points), 2)
+        print(sort_points(points))
+        points = []
+
+    pygame.display.flip()
