ded2765ea674e7945f891c8ce3bff72ad2cc1df7
DanielMilesGH <daniel@milesfamily.name>
2024-07-02 20:41:58 -0500

    made an easier formatting for points and lines

--- src/python/generate_object_borders.py
+++ src/python/generate_object_borders.py
@@ -1,11 +1,12 @@
+# TODO: grab the data from some other python file
 # this function is designed to be called by flask
 def generate_object_borders() -> dict:
     # TODO - ADD ACTUAL LOGIC 
     
     jsonToSend = {}
     
-    # borders in LineString WKT notation
-    bordersToSend = ["(0 0, 100 100, 200 100)"]
+    # borders are lines, in (x1,y1,x2,y2) notation
+    bordersToSend = ["(0,0,100,100)", "(100,100,200,100)"]
     
     jsonToSend['borders'] = bordersToSend
     
--- src/python/move_generation.py
+++ src/python/move_generation.py
@@ -1,1 +1,13 @@
 # TODO: grab the data from some other python file
+# this function is designed to be called by flask
+def move_generation():
+    # TODO - ADD ACTUAL LOGIC 
+    
+    jsonToSend = {}
+    
+    # moves in (x,y), where increasing goes right,down
+    movesToSend = ["(0,0)","(0,5)","(0,10)","(5,10)"]
+    
+    jsonToSend['moves'] = movesToSend
+    
+    return jsonToSend
